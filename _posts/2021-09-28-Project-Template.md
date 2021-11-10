4c5b6a63a8ff92ead593243336e31b4e90177415
King_Hornet <robertzacchigna@gmail.com>
2021-11-09 23:23:06 -0600

    Site redesign and loading posts

--- _posts/2021-09-28-Project-Template.md
+++ _posts/2021-09-28-Project-Template.md
@@ -1,12 +1,19 @@
 ---
 layout: post
-title: Project Title
+title: Project Title Here (Sample)
+tags: These, are, post-tags
+category: These are, post-categories
+summary: This is a sample post, I hope you like it.
 ---
 
-Other Project Content here
+Info about the jupyter notebook...
 
 {% assign part = "" %}
 {% assign notebook_page = "" %}
 {% assign github_notebook = "" %}
 
 {% include view-notebook.html html=content %}
+
+# Overview
+
+Other Project Content here...
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,30 @@
-<!-- Nav Bar Scroll Padding to Page Anchors -->
+// Nav Bar Scroll Padding to Page Anchors
 $(window).bind('load resize', function(event) {
   document.styleSheets[2].cssRules[0].style.setProperty("--nav-height", document.getElementById("nav-head").offsetHeight + 'px');
 });
 
-<!-- Nav Bar Page Highlighting -->
+// Nav Bar Page Highlighting
 $(function(){
-    $('a').each(function(){
+    $('.nav a').each(function(){
         if ($(this).prop('href') == window.location.href) {
             $(this).parent('div').addClass('active');
+        }
+    });
+});
+
+// Year Dropdown Buttons
+$(function(){
+    $(".dropdown-btn").click(function() {
+        $('.dropdown-btn').not(this).removeClass('active-btn');
+        $(this).toggleClass('active-btn');
+
+        var div_list = document.getElementsByClassName('sidebar-sticky')[0].getElementsByTagName('div');
+
+        for (var i = 0; i < div_list.length; i++) {
+            if (this.id === div_list[i].id) {
+                $('.dropdown-year').not(div_list[i]).removeClass('active-year');
+                $(div_list[i]).toggleClass('active-year');
+            }
         }
     });
 });
