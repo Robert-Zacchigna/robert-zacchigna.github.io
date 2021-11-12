3355d3b289165b92f9be394f49e70d7d3c43c1b0
King_Hornet <robertzacchigna@gmail.com>
2021-11-12 00:22:03 -0600

    Rewording of how to search the site (Home), search post counter (Search Posts), clear input on page refresh (Contact)

---
+++ assets/js/search-posts.js
@@ -0,0 +1,33 @@
+// Clear search input on page refresh
+$(function() {
+    document.getElementById('search-input').value = "";
+});
+
+// Generate post list to be searched
+window.simpleJekyllSearch = new SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: 'https://robert-zacchigna.github.io/Portfolio/search.json', // Manually set full website path of json file
+    searchResultTemplate: '<li><a href="{url}">{title}</a><p><strong>Summary:</strong> {summary} <br> <strong>Posted:</strong> {date} <br> <strong>Tag(s):</strong> {tags} <br> <strong>Categories:</strong> {category}</p></li>',
+    noResultsText: '<strong>No Results Found</strong>',
+    limit: 10,
+    fuzzy: false
+});
+
+// Dynamically count posts and append to title (i.e. "Search Posts (Results: 5)")
+const search_input = document.querySelector('#search-input');
+const page_title = document.querySelector('.page-title');
+
+search_input.addEventListener('input', function() {
+    setTimeout(function() {
+        var search_results = document.querySelector("#results-container").childNodes
+
+        if (typeof search_results[0] === "undefined") {
+            page_title.innerHTML = "Search Posts";
+        } else if (search_results[0].textContent === "No Results Found") {
+            page_title.innerHTML = "Search Posts (Results: 0)";
+        } else {
+            page_title.innerHTML = "Search Posts (Results: " + search_results.length + ")";
+        }
+    }, 265);
+});
--- search.md
+++ search.md
@@ -3,4 +3,18 @@ layout: page
 title: Search Posts
 ---
 
-{% include search.html html=content %}
+<!-- HTML elements for search -->
+<div id="fs-frm" style="padding-top: 1em;">
+    <center>
+        <input style="text-align: center;" type="text" id="search-input" maxlength="100" placeholder="Search By: Title, Tag, Category, Date...">
+    </center>
+</div>
+
+<ul id="results-container"></ul>
+
+<!-- script pointing to jekyll-search.js -->
+<script type="text/javascript" src="{{ "/assets/js/simple-jekyll-search.js" | prepend: site.baseurl }}"></script>
+<script type="text/javascript" src="{{ "/assets/js/simple-jekyll-search.min.js" | prepend: site.baseurl }}"></script>
+
+<!-- Generate posts list and count on search -->
+<script type="text/javascript" src="{{ "/assets/js/search-posts.js" | prepend: site.baseurl }}"></script>
