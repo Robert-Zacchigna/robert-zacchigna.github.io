ed0683be4c5ccb21ee3cb32ec7177eb5e5aef38f
King_Hornet <robertzacchigna@gmail.com>
2021-11-15 14:45:56 -0600

    Fix Post Results Counter

--- assets/js/search-posts.js
+++ assets/js/search-posts.js
@@ -4,11 +4,18 @@ $(function() {
 });
 
 // Generate post list to be searched
+var zipped_projects_link = 'https://github.com/Robert-Zacchigna/Portfolio/raw/Zipped-Projects/{title}.zip'
+
 window.simpleJekyllSearch = new SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container'),
     json: 'https://robert-zacchigna.github.io/Portfolio/search.json', // Manually set full website path of json file
-    searchResultTemplate: '<li><a href="{url}">{title}</a><p><strong>Summary:</strong> {summary} <br> <strong>Date:</strong> {date} <br> <strong>Tag(s):</strong> {tags} <br> <strong>Categories:</strong> {category} <br> <strong>Download Project:</strong> <a href="https://github.com/Robert-Zacchigna/Portfolio/raw/Zipped-Projects/{title}.zip">{download_size} - Zip Archive</a></p></li>',
+    searchResultTemplate: ('<li><a href="{url}">{title}</a><p>' +
+                           '<strong>Summary:</strong> {summary} <br>' +
+                           '<strong>Date:</strong> {date} <br>' +
+                           '<strong>Tag(s):</strong> {tags} <br>' +
+                           '<strong>Categories:</strong> {category} <br>' +
+                           '<strong>Download Project:</strong> <a href="' + zipped_projects_link + '">{download_size} - Zip Archive</a></p></li>'),
     noResultsText: '<strong>No Results Found</strong>',
     limit: 10,
     fuzzy: false
@@ -18,10 +25,10 @@ window.simpleJekyllSearch = new SimpleJekyllSearch({
 const search_input = document.querySelector('#search-input');
 const page_title = document.querySelector('.page-title');
 
-search_input.addEventListener('input', function() {
-    setTimeout(function() {
-        var search_results = document.querySelector("#results-container").childNodes
+var search_results = document.querySelector("#results-container").childNodes
 
+search_input.addEventListener('input', function() {
+    setInterval(function() {
         if (typeof search_results[0] === "undefined") {
             page_title.innerHTML = "Search Posts";
         } else if (search_results[0].textContent === "No Results Found") {
